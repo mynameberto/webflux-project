3d4bff087faa2495000f169bce4445e7642a6775
mynameberto <pedroobertasso@gmail.com>
2024-03-13 22:25:47 -0300

    add exception handler to object not found

--- src/main/java/br/com/bertasso/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/bertasso/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.bertasso.webfluxcourse.controller.exceptions;
 
+import br.com.bertasso.webfluxcourse.service.exception.ObjectNotFoundException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -20,8 +22,9 @@ public class ControllerExceptionHandler {
             DuplicateKeyException ex, ServerHttpRequest request
     ) {
         return ResponseEntity.badRequest()
-                .body(Mono.just(StandardError
-                        .builder()
+                .body(Mono.just(
+                        StandardError
+                                .builder()
                                 .timestamp(now())
                                 .status(BAD_REQUEST.value())
                                 .error(BAD_REQUEST.getReasonPhrase())
@@ -45,6 +48,24 @@ public class ControllerExceptionHandler {
         }
         return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
     }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    ResponseEntity<Mono<StandardError>> objectNotFoundException(
+            ObjectNotFoundException ex, ServerHttpRequest request
+    ) {
+        return ResponseEntity.status(NOT_FOUND)
+                .body(Mono.just(
+                        StandardError
+                                .builder()
+                                .timestamp(now())
+                                .status(NOT_FOUND.value())
+                                .error(NOT_FOUND.getReasonPhrase())
+                                .message(ex.getMessage())
+                                .path(request.getPath().toString())
+                                .build()
+                ));
+    }
+
     private String verifyDupKey(String message) {
         if (message.contains("email dup key")) {
             return "E-mail already registered";
