b8faaf27b93c6aaefcdbe8f6c2f58ae5b2fc2408
mynameberto <pedroobertasso@gmail.com>
2024-03-13 22:17:19 -0300

    add impl to contract findById user

--- src/main/java/br/com/bertasso/webfluxcourse/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/bertasso/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -1,6 +1,7 @@
 package br.com.bertasso.webfluxcourse.controller.impl;
 
 import br.com.bertasso.webfluxcourse.controller.UserController;
+import br.com.bertasso.webfluxcourse.mapper.UserMapper;
 import br.com.bertasso.webfluxcourse.model.request.UserRequest;
 import br.com.bertasso.webfluxcourse.model.response.UserResponse;
 import br.com.bertasso.webfluxcourse.service.UserService;
@@ -18,6 +19,7 @@ import reactor.core.publisher.Mono;
 public class UserControllerImpl implements UserController {
 
     private final UserService service;
+    private final UserMapper mapper;
 
     @Override
     public ResponseEntity<Mono<Void>> save(final UserRequest request) {
@@ -26,8 +28,10 @@ public class UserControllerImpl implements UserController {
     }
 
     @Override
-    public ResponseEntity<Mono<UserResponse>> find(String id) {
-        return null;
+    public ResponseEntity<Mono<UserResponse>> findById(String id) {
+        return ResponseEntity.ok().body(
+                service.findById(id).map(mapper::toResponse)
+        );
     }
 
     @Override
