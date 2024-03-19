ca1ea6e32ea5fa3822eb8a2aa4a0b780c99d0e53
mynameberto <pedroobertasso@gmail.com>
2024-03-18 21:30:28 -0300

    add validation constraint

--- src/main/java/br/com/bertasso/webfluxcourse/validator/TrimString.java
+++ src/main/java/br/com/bertasso/webfluxcourse/validator/TrimString.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Constraint(validatedBy = {  })
+@Constraint(validatedBy = { TrimStringValidator.class })
 @Target(FIELD)
 @Retention(RUNTIME)
 public @interface TrimString {
